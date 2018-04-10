1b6dfcb9c37433b3365c6f8900a9a085c230950a
itsnotcowitsbeef <ballaranjulius030@gmail.com>
2018-04-10 18:33:51 +0800

    Update Add forget password and validation script

---
+++ validation_form.php
@@ -0,0 +1,11 @@
+<?php 
+
+    //validation function
+    //returns validated input
+    function inputValidation($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        
+        return($data);
+    }
